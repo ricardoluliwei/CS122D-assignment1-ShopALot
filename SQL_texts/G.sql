40160949984c32976d098fc0647bb173614c1225
Liwei Lu <ricardoluliwei@gmail.com>
2021-04-15 15:39:55 -0700

    F

---
+++ SQL_texts/G.sql
@@ -0,0 +1,8 @@
+-- Continuing from the previous problem, use the UNNEST operator of PostgreSQL to create a normalized list with the store ID, 
+-- the name, and the category for each store with a zip code of 44401. 
+-- (Keep the list entries for each business together by using an ORDER BY clause.)
+
+SELECT s.store_id, s.name, category
+FROM stores s, UNNEST(string_to_array(s.categories, ', ')) as category
+WHERE s.zip_code = '44401'
+ORDER BY s.store_id

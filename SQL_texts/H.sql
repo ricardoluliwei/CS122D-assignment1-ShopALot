8613607467da26cd12f307f3efa5fea1dd3ac52f
Liwei Lu <ricardoluliwei@gmail.com>
2021-04-15 16:14:45 -0700

    H

--- SQL_texts/H.sql
+++ SQL_texts/H.sql
@@ -2,6 +2,16 @@
 -- as well as the number of stores that include each such category and the minimum, maximum, 
 -- and average list prices of the products in each such category.
 
-
-SELECT category
+WITH category_count AS (
+SELECT category, COUNT (DISTINCT s.store_id) as num_stores
 FROM stores s, UNNEST(string_to_array(s.categories, ', ')) as category
+GROUP BY category
+ORDER BY num_stores DESC
+LIMIT 5
+)
+
+SELECT cc.category, cc.num_stores, MIN(p.list_price), MAX(p.list_price), AVG(p.list_price)
+FROM category_count AS cc, products AS p
+WHERE cc.category = p.category
+GROUP BY cc.category, cc.num_stores
+ORDER BY cc.num_stores DESC

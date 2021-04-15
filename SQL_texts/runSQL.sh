9688a5437e58bc109b3b38686524e0ba5a488423
Liwei Lu <ricardoluliwei@gmail.com>
2021-04-15 12:48:41 -0700

    shell script

---
+++ SQL_texts/runSQL.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+
+if [[ $# != 0] && [$1 == "clean" ]]
+then 
+    rm -rf Output
+    exit
+fi
+
+if [[ $# != 0] && [$1 == "init" ]]
+then 
+    psql shopalot -h localhost -f create_database.sql
+    exit
+fi
+
+if [[ $# != 0] && [$1 == "load" ]]
+then 
+    psql shopalot -h localhost -f load_table.sql
+    exit
+fi
+
+if [ ! -d Output ]
+then 
+    mkdir Output
+fi
+
+for i in A B C D E F G H I J K L M
+do
+    psql shopalot -h localhost -f "$i.sql" > "Output/($i)Output.txt"
+done
+

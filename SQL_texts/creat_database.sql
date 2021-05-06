3e987b1dcf4429f5da4726224b1528ffa7e03a4d
Liwei Lu <ricardoluliwei@gmail.com>
2021-05-05 19:49:25 -0700

    final version

--- SQL_texts/creat_database.sql
+++ SQL_texts/creat_database.sql
@@ -1,6 +1,23 @@
 -- Entity tables below: --
+DROP TABLE IF EXISTS StockedBy; 
+DROP TABLE IF EXISTS Own; 
+DROP TABLE IF EXISTS WorkFor; 
+
+DROP TABLE IF EXISTS UserPhone;
+DROP TABLE IF EXISTS OrderItems; 
+
+DROP TABLE IF EXISTS Orders; 
+
+DROP TABLE IF EXISTS Customers; 
+DROP TABLE IF EXISTS Shoppers; 
+
+DROP TABLE IF EXISTS Vehicles; 
+DROP TABLE IF EXISTS Stores; 
+DROP TABLE IF EXISTS Products;
+
 
 DROP TABLE IF EXISTS Users; 
+
 CREATE TABLE Users (
     user_id char(5) PRIMARY KEY, 
     email varchar(100), 
@@ -8,18 +25,15 @@ CREATE TABLE Users (
     last_name varchar(50) NOT NULL
 );
 
-DROP TABLE IF EXISTS Customers; 
 CREATE TABLE Customers (
     user_id char(5) PRIMARY KEY
 );
 
-DROP TABLE IF EXISTS Shoppers; 
 CREATE TABLE Shoppers (
     user_id char(5) PRIMARY KEY,  
     capacity INTEGER 
 );
 
-DROP TABLE IF EXISTS Vehicles; 
 CREATE TABLE Vehicles (
     state char(2) NOT NULL, 
     license_plate varchar(15) NOT NULL, 
@@ -30,7 +44,6 @@ CREATE TABLE Vehicles (
     PRIMARY KEY (state, license_plate) 
 );
 
-DROP TABLE IF EXISTS Stores; 
 CREATE TABLE Stores (
     store_id varchar(5) PRIMARY KEY, 
     name varchar(50), 
@@ -42,7 +55,6 @@ CREATE TABLE Stores (
     categories varchar(300)
 );
 
-DROP TABLE IF EXISTS Products;
 CREATE TABLE Products(
     product_id char(5) PRIMARY KEY, 
     category varchar(100) NOT NULL, 
@@ -51,7 +63,6 @@ CREATE TABLE Products(
     list_price decimal(9,2) 
 );
 
-DROP TABLE IF EXISTS Orders; 
 CREATE TABLE Orders (
     order_id char(5) PRIMARY KEY, 
     total_price decimal(9,2), 
@@ -67,7 +78,6 @@ CREATE TABLE Orders (
 
 -- Relationship tables below: --
 
-DROP TABLE IF EXISTS UserPhone;
 CREATE TABLE UserPhone(
     user_id char(5), 
     type varchar(10), 
@@ -75,7 +85,6 @@ CREATE TABLE UserPhone(
     PRIMARY KEY (user_id, number)     
 );
 
-DROP TABLE IF EXISTS OrderItems; 
 CREATE TABLE OrderItems (
     item_id char(5), 
     qty INTEGER, 
@@ -85,7 +94,6 @@ CREATE TABLE OrderItems (
     PRIMARY KEY (item_id, order_id) 
 ); 
 
-DROP TABLE IF EXISTS StockedBy; 
 CREATE TABLE StockedBy (
     product_id char(5), 
     store_id char(5), 
@@ -93,7 +101,6 @@ CREATE TABLE StockedBy (
     PRIMARY KEY (product_id, store_id) 
 );
 
-DROP TABLE IF EXISTS Own; 
 CREATE TABLE Own (
     state char(2), 
     license_plate char(15), 
@@ -101,7 +108,6 @@ CREATE TABLE Own (
     PRIMARY KEY (state, license_plate, user_id) 
 );
 
-DROP TABLE IF EXISTS WorkFor; 
 CREATE TABLE WorkFor (
     store_id char(5), 
     shopper_id char(5),
